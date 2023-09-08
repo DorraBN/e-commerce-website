4cc57c2e6a9d89b4b09af5646bf9fa0ca703c1b3
DorraBN <dorransira18@gmail.com>
2023-09-08 23:57:55 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import * as html2canvas from 'html2canvas';
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent {
+  imagePath: string = 'assets/planet.jpg';
+  imagePath2: string = 'assets/horse.jpg';
+  imagePath3: string = 'assets/teddy.jpg';
+  imagePath8: string = 'assets/minion.jpg';
+  imagePath9: string = 'assets/barbie.jpg';
+  imagePath10: string = 'assets/planet.jpg';
+  imagePath4: string = 'assets/baby.jpg';
+  imagePath5: string = 'assets/b1.jpg';
+  imagePath6: string = 'assets/b2.jpg';
+  imagePath7: string = 'assets/b3.jpg';
+}
